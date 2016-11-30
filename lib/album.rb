571dec1161f881f75f75f87f31e19dbe430bbcc5
Adam Zinder and Tyler Ferrin <adam@>
2016-11-30 14:52:48 -0800

    use backend album class

--- lib/album.rb
+++ lib/album.rb
@@ -8,5 +8,26 @@ class Album
     @format = attributes.fetch(:format)
     @quality = attributes.fetch(:quality)
     @id = @@albums.length().+(1)
-  end  
+  end
+  def id
+    @id
+  end
+  def self.all
+    @@albums
+  end
+  def self.clear
+    @@albums = []
+  end
+  def save
+    @@albums.push(self)
+  end
+  def find(id)
+    found_album = nil
+    @@albums.each() do |album|
+      if album.id().eql?(id)
+        found_album = album
+      end
+    end
+    found_album
+  end
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -3,6 +3,9 @@ require('pry')
 require('album')
 
 describe(Album) do
+  before() do
+    Album.clear()
+  end
   describe('#title') do
     it("returns the name of an album object") do
       album1 = Album.new(:title => "Some Girls", :format => "LP", :quality => "Very Good")
@@ -21,6 +24,32 @@ describe(Album) do
     it("returns the quality of the album object") do
       album1 = Album.new(:title => "Some Girls", :format => "LP", :quality => "Very Good")
       expect(album1.quality()).to(eq("Very Good"))
+    end
+  end
+  describe("#id") do
+    it("returns the associated id number with a new instance") do
+      album1 = Album.new(:title => "Some Girls", :format => "LP", :quality => "Very Good")
+      expect(album1.id()).to(eq(1))
+    end
+  end
+  describe('.all') do
+    it('will return all album instances') do
+        expect(Album.all()).to(eq([]))
+    end
+  end
+  describe('save') do
+    it('it will save an instance') do
+      album1 = Album.new(:title => "Some Girls", :format => "LP", :quality => "Very Good")
+      album1.save()
+      expect(Album.all()).to(eq([album1]))
+    end
+  end
+  describe('clear') do
+    it("will clear all instances from album class array") do
+      album1 = Album.new(:title => "Some Girls", :format => "LP", :quality => "Very Good")
+      album1.save()
+      Album.clear()
+      expect(Album.all()).to(eq([]))
     end
   end
 end
